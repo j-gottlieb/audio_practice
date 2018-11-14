583c17af9b7869de0b4d1615d4496268b9f27c4b
j-gottlieb <gessejottlieb@gmail.com>
2018-11-14 12:56:44 -0500

    fixed volume slider
    added nsync

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,7 +2,7 @@
 
 // simple button to play a song
 const playSound = function () {
-  const jean = new Audio('../../../audio/jean.mp3')
+  const jean = new Audio('../../../audio/nsync.mp3')
   if (jean.paused) {
     jean.currentTime = 0
     jean.play()
@@ -22,10 +22,10 @@ const gainNode = audioContext.createGain()
 // connect the signal chain
 oscillator.connect(distortion)
 distortion.connect(gainNode)
-distortion.connect(analyser)
+gainNode.connect(analyser)
 // gainNode.connect(audioContext.destination)
 
-// oscillator.frequency.value = 200
+gainNode.gain.value = 0.12
 
 const start = function () {
   oscillator.start()
@@ -39,62 +39,60 @@ const stop = function () {
 
 oscillator.start()
 
-analyser.fftSize = 2048;
-var bufferLength = analyser.frequencyBinCount;
-var dataArray = new Uint8Array(bufferLength);
-analyser.getByteTimeDomainData(dataArray);
+analyser.fftSize = 2048
+const bufferLength = analyser.frequencyBinCount
+const dataArray = new Uint8Array(bufferLength)
+analyser.getByteTimeDomainData(dataArray)
 
 // Get a canvas defined with ID "oscilloscope"
-var canvas = document.getElementById("oscilloscope");
-var canvasCtx = canvas.getContext("2d");
+const canvas = document.getElementById("oscilloscope")
+const canvasCtx = canvas.getContext("2d")
 
 // draw an oscilloscope of the current audio source
 
-function draw() {
-
-  requestAnimationFrame(draw);
-
-  analyser.getByteTimeDomainData(dataArray);
-
-  canvasCtx.fillStyle = "rgb(200, 200, 200)";
-  canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
-
-  canvasCtx.lineWidth = 2;
-  canvasCtx.strokeStyle = "rgb(0, 0, 0)";
-
-  canvasCtx.beginPath();
-
-  var sliceWidth = canvas.width * 1.0 / bufferLength;
-  var x = 0;
-
-  for (var i = 0; i < bufferLength; i++) {
-
-    var v = dataArray[i] / 128.0;
-    var y = v * canvas.height / 2;
-
-    if (i === 0) {
-      canvasCtx.moveTo(x, y);
-    } else {
-      canvasCtx.lineTo(x, y);
-    }
-
-    x += sliceWidth;
-  }
-
-  canvasCtx.lineTo(canvas.width, canvas.height / 2);
-  canvasCtx.stroke();
-}
-draw()
+// function draw () {
+//
+//   requestAnimationFrame(draw);
+//
+//   analyser.getByteTimeDomainData(dataArray);
+//
+//   canvasCtx.fillStyle = "rgb(200, 200, 200)";
+//   canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+//
+//   canvasCtx.lineWidth = 2;
+//   canvasCtx.strokeStyle = "rgb(0, 0, 0)";
+//
+//   canvasCtx.beginPath();
+//
+//   var sliceWidth = canvas.width * 1.0 / bufferLength;
+//   var x = 0;
+//
+//   for (var i = 0; i < bufferLength; i++) {
+//
+//     var v = dataArray[i] / 128.0;
+//     var y = v * canvas.height / 2;
+//
+//     if (i === 0) {
+//       canvasCtx.moveTo(x, y);
+//     } else {
+//       canvasCtx.lineTo(x, y);
+//     }
+//
+//     x += sliceWidth;
+//   }
+//
+//   canvasCtx.lineTo(canvas.width, canvas.height / 2);
+//   canvasCtx.stroke();
+// }
+// draw()
 
 $(() => {
   $('#test').on('click', playSound)
   $('#start').click(function () {
     analyser.connect(audioContext.destination)
-    console.log(analyser)
   })
   $('#stop').click(function () {
     analyser.disconnect(audioContext.destination)
-      console.log(analyser)
   })
   $('#synth-volume').on('input', function () {
     gainNode.gain.value = this.value
