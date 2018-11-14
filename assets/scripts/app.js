57e8d444a90b924ecafa4b060e33607b020a152c
j-gottlieb <gessejottlieb@gmail.com>
2018-11-14 12:10:51 -0500

    Added oscilloscope

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -22,6 +22,7 @@ const gainNode = audioContext.createGain()
 // connect the signal chain
 oscillator.connect(distortion)
 distortion.connect(gainNode)
+distortion.connect(analyser)
 // gainNode.connect(audioContext.destination)
 
 // oscillator.frequency.value = 200
@@ -38,13 +39,62 @@ const stop = function () {
 
 oscillator.start()
 
+analyser.fftSize = 2048;
+var bufferLength = analyser.frequencyBinCount;
+var dataArray = new Uint8Array(bufferLength);
+analyser.getByteTimeDomainData(dataArray);
+
+// Get a canvas defined with ID "oscilloscope"
+var canvas = document.getElementById("oscilloscope");
+var canvasCtx = canvas.getContext("2d");
+
+// draw an oscilloscope of the current audio source
+
+function draw() {
+
+  requestAnimationFrame(draw);
+
+  analyser.getByteTimeDomainData(dataArray);
+
+  canvasCtx.fillStyle = "rgb(200, 200, 200)";
+  canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+
+  canvasCtx.lineWidth = 2;
+  canvasCtx.strokeStyle = "rgb(0, 0, 0)";
+
+  canvasCtx.beginPath();
+
+  var sliceWidth = canvas.width * 1.0 / bufferLength;
+  var x = 0;
+
+  for (var i = 0; i < bufferLength; i++) {
+
+    var v = dataArray[i] / 128.0;
+    var y = v * canvas.height / 2;
+
+    if (i === 0) {
+      canvasCtx.moveTo(x, y);
+    } else {
+      canvasCtx.lineTo(x, y);
+    }
+
+    x += sliceWidth;
+  }
+
+  canvasCtx.lineTo(canvas.width, canvas.height / 2);
+  canvasCtx.stroke();
+}
+draw()
+
 $(() => {
   $('#test').on('click', playSound)
   $('#start').click(function () {
-    gainNode.connect(audioContext.destination)
+    analyser.connect(audioContext.destination)
+    console.log(analyser)
   })
   $('#stop').click(function () {
-    gainNode.disconnect(audioContext.destination)
+    analyser.disconnect(audioContext.destination)
+      console.log(analyser)
   })
   $('#synth-volume').on('input', function () {
     gainNode.gain.value = this.value
@@ -54,5 +104,8 @@ $(() => {
   })
   $('#shape-selector').on('change', function () {
     oscillator.type = this.value
+  })
+  $('#toggle-oscillator').on('click', function () {
+    $('#oscilloscope').toggle()
   })
 })
